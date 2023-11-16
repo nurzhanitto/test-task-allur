af5f22dad382347b96fdfd9ea8dfea82272fb07c
Nurzhan Ilyasov <nurzanilasov@gmail.com>
2023-11-16 10:36:48 +0600

    Added Button component and some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
+import { CalculatorOutlined } from '@ant-design/icons';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <>
+    <div className="content">
+      <div className='content-app'>
+        <h1>Заявления</h1>
+        <Button icon={<CalculatorOutlined />}>Калькулятор</Button>
+      </div>
     </div>
-  );
+  </>
 }
 
 export default App;
